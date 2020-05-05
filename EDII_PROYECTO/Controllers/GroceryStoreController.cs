d67ffdd62fdcd96399660663f1e9fb38b7b03628
PabloArreaga <paal241199@gmail.com>
2020-05-04 19:27:25 -0600

    Creacion de controlador y clases de producto, sucursal y sucursal-producto

---
+++ EDII_PROYECTO/Controllers/GroceryStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDII_PROYECTO.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class GroceryStoreController : ControllerBase
+    {
+        private static readonly string prueba = "Hola mundo, Esto será modificado en el futuro";
+    }
+}
---
+++ EDII_PROYECTO/Models/Store_Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDII_PROYECTO.Models
+{
+	public class Store_Product
+	{
+		int IdStore { get; set; }
+		int IdProduct { get; set; }
+		int Stock { get; set; }
+
+	}
+}
